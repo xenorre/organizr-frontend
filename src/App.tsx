09ef6fde65b5f833cdcee12a8dcac158148c6ed1
Dawid <corre14@gmail.com>
2025-08-31 23:21:00 +0200

    chore: initial commit with frontend boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <header className="p-4 bg-white shadow">
+        <h1 className="text-xl font-bold">Organizr</h1>
+      </header>
+      <main className="p-6"></main>
+    </div>
+  );
+}
